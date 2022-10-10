926990f61f60efad8949ae702006cefcd867867f
codydematth <codydematth@gmail.com>
2022-10-10 22:16:47 +0100

    Update CKEDITOR

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "blog.apps.BlogConfig",
     "users.apps.UsersConfig",
     "crispy_forms",
+    "ckeditor",
+    "autoslug",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
