cd54be3f6c554790c44d85ae6d65a04dfd89c8fb
codydematth <codydematth@gmail.com>
2022-10-10 12:23:36 +0100

    MODEL Updates

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = RichTextField(null=True, blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -14,16 +15,17 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        return reverse("post-detail", kwargs={"pk": self.pk})
+
 
 class Comment(models.Model):
-	post = models.ForeignKey(Post, on_delete=models.CASCADE)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	content = models.TextField( max_length=300)
-	date_posted = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    date_posted = models.DateTimeField(default=timezone.now)
 
-	def __str__(self):
-		return '{} --- {}'.format(self.post.title, str(self.author.username))
+    def __str__(self):
+        return "{} --- {}".format(self.post.title, str(self.author.username))
 
-	def get_absolute_url(self):
-		return reverse('post-detail', kwargs={'pk': self.pk})
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
