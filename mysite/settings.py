b827317c96410f2636d7a1a4cf0648f993662fea
codydematth <codydematth@gmail.com>
2023-01-12 02:02:01 +0100

    reployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["cody-blog-post.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 # "127.0.0.1",
 
 if DEBUG:
