987b4299c70d21cd13e66ad5187459c83345db3e
Oluwafemi Mathias A <36882040+codydematth@users.noreply.github.com>
2023-01-12 02:39:49 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1","students-blog-post.up.railway.app"]
 # "127.0.0.1",
 
 if DEBUG:
@@ -88,7 +88,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
         "USER": "postgres",
-        "PASSWORD": "xXnm2RMheHKkqLUhS5KT",
+        "PASSWORD": "yO5YpxHFEPsBMTuRzKNk",
         "HOST": "containers-us-west-84.railway.app",
         "PORT": "7880",
     }
